7cbd55da407941dbc8667bf6319c359ee3ee0293
AcornJavascript <qkdnf325@daum.net>
2024-12-26 10:14:22 +0900

    커밋1

---
+++ src/main/java/com/acorn/acorn_server1226_2/HelloController.java
@@ -0,0 +1,15 @@
+package com.acorn.acorn_server1226_2;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+
+    @GetMapping("/data")
+    public String hi(){
+        return "hi DATA";
+    }
+}
